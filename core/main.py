d199ab2e809f8f2f220cd159b612e8789e6ee4b9
Franc963 <franc.turnar@gmail.com>
2026-01-21 22:59:26 +0100

    Create main.py

---
+++ core/main.py
@@ -0,0 +1,13 @@
+from jedro.phone_detector import is_suspicious_phone
+
+def main():
+    print("🔍 Detektor prevar – zagon")
+
+    phone = input("Vnesi telefonsko številko: ")
+    if is_suspicious_phone(phone):
+        print("⚠️ POZOR: Možna telefonska prevara!")
+    else:
+        print("✅ Telefonska številka ni zaznana kot prevara.")
+
+if __name__ == "__main__":
+    main()
